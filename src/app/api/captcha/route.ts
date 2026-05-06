2ab071f345e15c904d5973e466a4f11d91928fdf
ROMAN111BETEH <94536768+ROMAN111BETEH@users.noreply.github.com>
2026-05-06 18:39:15 +0300

    Delete src directory

--- src/app/api/captcha/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { createCaptchaChallenge } from "@/lib/captcha";
-
-export async function GET() {
-  return NextResponse.json(createCaptchaChallenge());
-}
